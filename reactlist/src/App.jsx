bbf118c7b71110891e114ebe576eb6391a6685f0
CAUHE SANCHES MATHEUS <54440848800@info.local>
2026-07-31 14:10:31 -0300

    atividade realizada

--- reactlist/src/App.jsx
+++ reactlist/src/App.jsx
@@ -1,69 +1,164 @@
 import './App.css'
-import { useState } from 'react'
-import Editar from "./assets/editar.svg"
-import Deletar from "./assets/deletar.svg"
+import { useEffect, useState } from 'react'
+import Editar from './assets/editar.svg'
+import Deletar from './assets/deletar.svg'
+import axios from 'axios'
 
 function App() {
-  //states
-  const [tasklist, setTasklist] = useState([
-    {id: 1, description: "estudar react"},
-    {id: 2, description: "estudar javascript"},
-    {id: 3, description: "estudar HTML"},
-    {id: 4, description: "estudar algo que eu não sei ainda e tbm não sei se vou aprender"},
-  ])
-  //effects
-  //funções
+  const [tasklist, setTasklist] = useState([])
+  const [taskvalue, setTaskvalue] = useState('')
+  const [editmode, setEditmode] = useState(false)
+  const [editingTaskId, setEditingTaskId] = useState(0)
 
+  useEffect(() => {
+    getTasklist()
+  }, [])
+
+  const getTasklist = async () => {
+    try {
+      const APIReturn = await axios.get('http://localhost:3000/taskpoint')
+      setTasklist(APIReturn.data)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  const postTasklist = async (item) => {
+    item.preventDefault()
+
+    if (taskvalue.trim().length == 0) {
+      alert('Digite algo para cadastrar')
+      return false
+    }
+
+    try {
+     const APIReturn = await axios.post('http://localhost:3000/taskpoint', { description: taskvalue })
+      await getTasklist()
+      setTaskvalue('')
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const putTasklist = async (item) => {
+    if (item?.preventDefault) {
+      item.preventDefault()
+
+      if (taskvalue.trim().length == 0) {
+        alert('Digite algo para editar')
+        return false
+      }
+
+      try {
+       const APIReturn = await axios.put(`http://localhost:3000/taskpoint/${editingTaskId}`, { description: taskvalue })
+        await getTasklist()
+        setTaskvalue('')
+        setEditmode(false)
+        setEditingTaskId(null)
+      } catch (error) {
+        console.error(error)
+      }
+
+      return
+    }
+
+    setTaskvalue(item.description)
+    setEditmode(true)
+    setEditingTaskId(item.id)
+  }
+
+  const deleteTasklist = async (item) => {
+    const resposta = confirm(`Deseja realmente deletar a tarefa ${item.description}?`)
+
+    if (!resposta) {
+      return false
+    }
+
+    try {
+      const APIReturn = await axios.delete(`http://localhost:3000/taskpoint/${item.id}`)
+      alert(`Tarefa ${item.description} deletada com sucesso`)
+      getTasklist()
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  {/*função q altera o post pro put*/}
+  const EditSubmit = (item) => {
+    if (editmode) {
+      return putTasklist(item)
+    }
+
+    return postTasklist(item)
+  }
 
   return (
     <>
+      <header className='header-section'>
+        <h1 className='header-section__title'>React List</h1>
+      </header>
 
-    <header className='header-section'>
+      <main className='body-section'>
+        <form className='cad-task' onSubmit={EditSubmit}>
+          <input
+            type='text'
+            placeholder='escreva alguma coisa'
+            className='card-task__entry'
+            value={taskvalue}
+            onChange={(e) => setTaskvalue(e.target.value)}
+          />
 
-     <h1 className='header-section__title'>React List</h1>
+          <button className='card-task__btn-confirm'>
+            {editmode ? 'Atualizar' : 'Cadastrar'}
+          </button>
 
-    </header>
+          {
 
-    <main className='body-section'>
+            editmode && (
+              <button className='card-task__btn-cancel' onClick={() => {
+                // cancela a edição
+                setTaskvalue('')
+                setEditmode(false)
+                setEditingTaskId(null)
+              }}>
+                Cancelar
+              </button>
+            )
+                  
+          }
 
-      <form className='cad-task'>
+        </form>
 
-        <input type="text" placeholder='escreva alguma coisa' className='card-task__entry' />
-
-        <button className='card-task__btn-confirm'>Cadastrar</button>
-
-      </form>
-
-      <section className='card-list'>
-
-        {
-          tasklist.map((task) => {
+        <section className='card-list'>
+          {tasklist.map((task) => {
             return (
               <article className='cardtask' key={task.id}>
                 <p>{task.description}</p>
                 <div className='icon-botton'>
-                  <a href="">
-                    <img src={Editar} alt="" />
-                  </a>
-                  <a href="">
-                    <img src={Deletar} alt="" />
-                  </a>
+                  <button
+                    type='button'
+                    className='icon-button'
+                    onClick={() => putTasklist(task)}
+                  >
+                    <img src={Editar} alt='imagem de um lapis com um papel, função de editar' />
+                  </button>
+                  <button
+                    type='button'
+                    className='icon-button'
+                    onClick={() => deleteTasklist(task)}
+                  >
+                    <img src={Deletar} alt='imagem de um lixeira, função de deletar' />
+                  </button>
                 </div>
               </article>
             )
-          })
-        }
+          })}
+        </section>
+      </main>
 
-      </section>
-
-    </main>
-
-    <footer className='footer-list'>
-
-      <p className='footer-list__right-text'>2026 - React List - Todos os direitos reservados</p>
-
-    </footer>
-
+      <footer className='footer-list'>
+        <p className='footer-list__right-text'>2026 - React List - Todos os direitos reservados</p>
+      </footer>
     </>
   )
 }
